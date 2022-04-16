309b9a6ebdf913980215f1af08b2fb066c312006
Gorgy <egorsasim@mail.ru>
2022-04-16 13:06:26 +0300

    open key cipher algorythm

---
+++ lab3/ElGamal.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+
+    
+
+    public  class ElGamal
+    {
+
+        public BigInteger p;
+        public BigInteger g;
+        public BigInteger x;
+        public BigInteger y;
+        public BigInteger k;
+        public BigInteger a;
+        public BigInteger b;
+        // Generate the prime nums list from selected range; created for generating "p";
+        public  List<long> getPrimes(long n)
+        {
+
+            bool[] is_prime = new bool[n + 1];
+            for (long i = 2; i <= n; ++i)
+                is_prime[i] = true;
+
+            List<long> primes = new List<long>();
+
+            for (long i = 2; i <= n; ++i)
+                if (is_prime[i])
+                {
+                    primes.Add(i);
+                    if (i * i <= n)
+                        for (long j = i * i; j <= n; j += i)
+                            is_prime[j] = false;
+                }
+
+            return primes;
+        }
+
+        // Return random value from passed arg(list<long>); need to generate P value;
+        public long getRandPrimeNum(List<long> primes, int p_min)
+        {
+            return primes[new Random().Next(p_min, primes.Count)];
+        }
+
+
+
+        //CHECK FOR PRIMAL NUMBER: БЫСТРОЕ ВОЗВЕДЕНИЕ В СТЕПЕНЬ ПО МОДУЛЮ;
+
+        // a - что возводим;
+        // b - во что возводим;
+        // c - модуль;
+        public  BigInteger FastExprModulo(BigInteger a, BigInteger b, BigInteger c)
+        {
+            BigInteger x = 1;
+            while (b != 0)
+            {
+                while (b % 2 == 0)
+                {
+                    b = BigInteger.Divide(b, 2);
+                    a = BigInteger.Multiply(a, a);
+                    a = BigInteger.Remainder(a, c);
+                }
+                b = BigInteger.Subtract(b, 1);
+                x = BigInteger.Multiply(x, a);
+                x = BigInteger.Remainder(x, c);
+            }
+            return x;
+        }
+        public  bool IsPrime(BigInteger n)
+        {
+            int k = 100;
+            if (n <= 100)
+                k = (int)n - 1;
+            for (int i = 2; i < k; i++)
+            {
+                BigInteger a = FastExprModulo(i, n - 1, n);
+                if(a != 1)
+                    return false;
+            }
+            return true;
+        }
+
+
+        public BigInteger FastExprModuloB(BigInteger a, BigInteger b, BigInteger c, BigInteger m)
+        {
+            BigInteger res = new BigInteger(1);
+
+            res = BigInteger.Multiply( BigInteger.Pow(a, (int)b), m ) % c;
+
+            return res;
+
+        }
+
+
+        // FIND PRIME ROOTS
+
+        public List<BigInteger> findPrimeRoots(BigInteger p)
+        {
+            List<BigInteger> primeRoots = new List<BigInteger>();
+               
+            bool isPrimeRoot;
+            for (BigInteger g = 1; g <= p - 1; g++)
+            {
+               
+                if (FastExprModulo(g, p-1,p) == 1)
+                {
+                    isPrimeRoot = true;
+                    for(BigInteger l = 1; l <= p - 2; l++)
+                    {
+                        if ( FastExprModulo(g, l, p) == 1)
+                        {
+                            isPrimeRoot=false;
+                            break;
+                        }
+                    }
+                    if (!isPrimeRoot)
+                        continue;
+                }
+                else
+                {
+                    continue;
+                }
+                
+                
+                if(isPrimeRoot)
+                    primeRoots.Add(g);
+            }
+          
+            return primeRoots;
+        }
+
+
+        public BigInteger generatePrimeRoot(BigInteger num)
+        {
+            bool isPrimeRoot;
+            do
+            {
+                isPrimeRoot = true;
+                Random rnd = new Random();
+
+                BigInteger g = getRandomBigInt( 2,  num - 1);
+
+                for (BigInteger l = 1; l <= num - 2; l++)
+                {
+                    if (FastExprModulo(g, l, num) == 1)
+                    {
+                        isPrimeRoot = false;
+                        break;
+                    }
+                }
+
+                if (isPrimeRoot == true)
+                    return g;
+
+            } while (isPrimeRoot == false);
+
+            return 0;
+        }
+
+        // GENERATE RANDOM BIGINTEGER
+
+
+        //public BigInteger getRandomBigInt(BigInteger start,  BigInteger finish)
+        //{
+        //    return new Random().Next( (int)start, (finish > 2147483647) ? 2147483647 : (int)finish);
+
+
+        //}
+
+
+        //public BigInteger getRandomBigInt(this Random random,
+        //BigInteger minValue, BigInteger maxValue)
+        //{
+        //    if (minValue > maxValue) throw new ArgumentException();
+        //    if (minValue == maxValue) return minValue;
+        //    BigInteger zeroBasedUpperBound = maxValue - 1 - minValue; // Inclusive
+        //    Debug.Assert(zeroBasedUpperBound.Sign >= 0);
+        //    byte[] bytes = zeroBasedUpperBound.ToByteArray();
+        //    Debug.Assert(bytes.Length > 0);
+        //    Debug.Assert((bytes[bytes.Length - 1] & 0b10000000) == 0);
+
+        //    // Search for the most significant non-zero bit
+        //    byte lastByteMask = 0b11111111;
+        //    for (byte mask = 0b10000000; mask > 0; mask >>= 1, lastByteMask >>= 1)
+        //    {
+        //        if ((bytes[bytes.Length - 1] & mask) == mask) break; // We found it
+        //    }
+
+        //    while (true)
+        //    {
+        //        random.NextBytes(bytes);
+        //        bytes[bytes.Length - 1] &= lastByteMask;
+        //        var result = new BigInteger(bytes);
+        //        Debug.Assert(result.Sign >= 0);
+        //        if (result <= zeroBasedUpperBound) return result + minValue;
+        //    }
+        //}
+
+        public BigInteger getRandomBigInt(BigInteger min, BigInteger max)
+        {
+            Random rnd = new Random();
+            string numeratorString, denominatorString;
+            double fraction = rnd.NextDouble();
+            BigInteger inRange;
+
+            //Maintain all 17 digits of precision, 
+            //but remove the leading zero and the decimal point;
+            numeratorString = fraction.ToString("G17").Remove(0, 2);
+
+            //Use the length instead of 17 in case the random
+            //fraction ends with one or more zeros
+            denominatorString = string.Format("1E{0}", numeratorString.Length);
+
+            inRange = (max - min) * BigInteger.Parse(numeratorString) /
+               BigInteger.Parse(denominatorString,
+               System.Globalization.NumberStyles.AllowExponent)
+               + min;
+            return inRange;
+        }
+
+        //
+
+        // GET NOD
+        public BigInteger GetNOD(BigInteger val1, BigInteger val2)
+        {
+            while ((val1 != 0) && (val2 != 0))
+            {
+                if ( BigInteger.Compare(val1, val2) > 0)
+                   val1 %= val2;
+                else
+                    val2 %= val1;
+            }
+            return (BigInteger.Compare(val1, val2) > 0 ? val1 : val2); //Math.Max((int)val1, (int)val2);
+        }
+    }
+}
---
+++ lab3/MainWindow.xaml.cs
@@ -0,0 +1,294 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public const long P_MAX = 3000;
+        public const int P_MIN = 3;
+        ElGamal elGam = new ElGamal();
+        public byte[] readenFile;
+        public byte[] cipheredData;
+        public byte[] aValue;
+        public byte[] bValue;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //MessageBox.Show(Convert.ToString( elGam.IsPrime(BigInteger.Parse("11111111111111111111111"))));
+            //MessageBox.Show(Convert.ToString(elGam.GetNOD(322328, 122120)));
+            
+            
+        }
+
+        private void generatePBtn_Click(object sender, RoutedEventArgs e)
+        {
+           
+            List<long> primes = elGam.getPrimes(P_MAX);
+            long randPrimeNum = elGam.getRandPrimeNum(primes, P_MIN);
+            pValueTxtBox.Text = Convert.ToString(randPrimeNum);
+
+        }
+
+        private void generatePrimeRoot_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bool isOk = elGam.IsPrime(BigInteger.Parse(pValueTxtBox.Text));
+                if (isOk)
+                {
+                    choosenRootTxtBox.Text = Convert.ToString(elGam.generatePrimeRoot(BigInteger.Parse(pValueTxtBox.Text)));
+                }
+                else
+                {
+                    MessageBox.Show("P is not prime");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Incorrect p");
+            }
+
+            
+            
+            //if (elGam.IsPrime(BigInteger.Parse(pValueTxtBox.Text))) 
+            //    choosenRootTxtBox.Text = Convert.ToString(elGam.generatePrimeRoot(BigInteger.Parse(pValueTxtBox.Text)));
+            //else
+            //{
+            //    MessageBox.Show("P is not prime");
+            //}
+        }
+
+        private void findPrimeRootsBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                bool isOk = elGam.IsPrime(BigInteger.Parse(pValueTxtBox.Text));
+                if (isOk)
+                {
+                    List<BigInteger> primeRoots = elGam.findPrimeRoots(BigInteger.Parse(pValueTxtBox.Text));
+                    MessageBox.Show(string.Join(",", primeRoots));
+                    primeRootsListBox.ItemsSource = primeRoots;
+                    rootCntTxtBox.Text = Convert.ToString(primeRoots.Count);
+                }
+                else
+                {
+                    MessageBox.Show("P is not prime");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Incorrect p");
+            }
+            
+            //if (pValueTxtBox.Text == "")
+            //{
+            //    MessageBox.Show("An empty P value.");
+            //}
+            //else
+            //{
+            //    if (elGam.IsPrime(BigInteger.Parse(pValueTxtBox.Text)))
+            //    {
+            //        List<BigInteger> primeRoots = elGam.findPrimeRoots(BigInteger.Parse(pValueTxtBox.Text));
+            //        MessageBox.Show(string.Join(",", primeRoots));
+            //        primeRootsListBox.ItemsSource = primeRoots;
+            //        rootCntTxtBox.Text = Convert.ToString(primeRoots.Count);
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("P is not prime");
+            //    }
+            //}
+
+            
+
+
+        }
+
+        private void primeRootsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            choosenRootTxtBox.Text = Convert.ToString( primeRootsListBox.SelectedItem );
+        }
+
+        private void generateClosedKeyBtn_Click(object sender, RoutedEventArgs e)
+        {
+           
+            BigInteger value = elGam.getRandomBigInt(new BigInteger(2), BigInteger.Subtract(BigInteger.Parse(pValueTxtBox.Text), 1));
+            closedKeyTextBox.Text = Convert.ToString(value);
+        }
+
+
+        private void generateYBtn_Click(object sender, RoutedEventArgs e)
+        {
+            yTxtBox.Text = Convert.ToString( elGam.FastExprModulo(BigInteger.Parse(choosenRootTxtBox.Text), BigInteger.Parse(closedKeyTextBox.Text), BigInteger.Parse(pValueTxtBox.Text)) );
+        }
+
+        private void generateKSecretNumBtn_Click(object sender, RoutedEventArgs e)
+        {
+            BigInteger k;
+            do
+            {
+                k = elGam.getRandomBigInt(new BigInteger(2), BigInteger.Subtract(BigInteger.Parse(pValueTxtBox.Text), 1));
+            } while (elGam.GetNOD(k, BigInteger.Subtract(BigInteger.Parse(pValueTxtBox.Text), 1)) != 1);
+
+            kSecretNumTxtBox.Text = Convert.ToString(k);
+        }
+
+        private void openFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                fileContentTxtBox.Text = string.Join(" ", File.ReadAllBytes(openFileDialog.FileName));
+                readenFile = File.ReadAllBytes(openFileDialog.FileName);
+            }
+                
+        }
+
+        private void generateABtn_Click(object sender, RoutedEventArgs e)
+        {
+            aValueTxtBox.Text = Convert.ToString(elGam.FastExprModulo( BigInteger.Parse(choosenRootTxtBox.Text), BigInteger.Parse(kSecretNumTxtBox.Text), BigInteger.Parse(pValueTxtBox.Text) ));
+            
+        }
+
+        private void generateBBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(fileContentTxtBox.Text != "")
+            {
+                bValueTxtBox.Text = "";
+                for (long i = 0; i < readenFile.Length; i++)
+                {
+                    bValueTxtBox.AppendText( (elGam.FastExprModuloB(BigInteger.Parse(yTxtBox.Text), BigInteger.Parse(kSecretNumTxtBox.Text), BigInteger.Parse(pValueTxtBox.Text), readenFile[i])).ToString() + "\n");
+                }
+            }
+            else
+            {
+                MessageBox.Show("At first open file, please.");
+            }
+           
+            
+        }
+
+        private void writeToCipheredFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(BigInteger.Compare(BigInteger.Parse(closedKeyTextBox.Text) ,1) == 0)
+            {
+                MessageBox.Show("Incorrect X");
+            }
+            
+            if(  (BigInteger.Compare( BigInteger.Parse(closedKeyTextBox.Text), BigInteger.Parse(pValueTxtBox.Text) ) < 0) ) 
+            {
+                cipheredFileContentTxtBox.Text = "";
+                for (int i = 0; i < bValueTxtBox.LineCount - 1; i++)
+                {
+                    cipheredFileContentTxtBox.AppendText(aValueTxtBox.Text + "," + bValueTxtBox.GetLineText(i));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Incorrect X value");
+            }
+
+            
+
+        }
+
+        private void saveCipheredText_Click(object sender, RoutedEventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                if ((myStream = saveFileDialog.OpenFile()) != null)
+                {
+                    
+
+                    myStream.Close();
+                }
+                File.WriteAllBytes(saveFileDialog.FileName, Encoding.ASCII.GetBytes(cipheredFileContentTxtBox.Text));
+            }
+        }
+
+
+
+        ///////////////////////////////////////////////////////// DECRIPTION ////////////////////////////////////////
+
+        public string[] readenCipheredFileCopy;
+        
+        public byte[] decriptedFile;
+        private void openCipheredFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                cipheredFileContentDecriptTxtBox.Text = File.ReadAllText(openFileDialog.FileName);//string.Join(" ", File.ReadAllBytes(openFileDialog.FileName));
+                readenCipheredFileCopy = cipheredFileContentDecriptTxtBox.Text.Split('\n',',');
+                
+            }
+
+           
+            
+        }
+
+        private void decriptBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                decipherFileContentTxtBox.Text = "";
+                BigInteger p = BigInteger.Parse(pDecriptValueTxtBox.Text);
+                BigInteger x = BigInteger.Parse(xDecriptValueTxtBox.Text);
+                decriptedFile = new byte[readenCipheredFileCopy.Length / 2];
+                for (long j = 0, i = 0; j < readenCipheredFileCopy.Length - 1; i++, j += 2)
+                {
+                    decriptedFile[i] = (byte)(BigInteger.Remainder(BigInteger.Multiply(BigInteger.Parse(readenCipheredFileCopy[j + 1]), BigInteger.Pow(BigInteger.Parse(readenCipheredFileCopy[j]), (int)BigInteger.Multiply(x, BigInteger.Subtract(p, 2)))), p));
+                    decipherFileContentTxtBox.AppendText( decriptedFile[i].ToString());
+                    
+                }
+            }
+            catch {
+                MessageBox.Show("Smth goes wrong");
+            }
+            
+        }
+
+        private void saveDecriptedFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                if ((myStream = saveFileDialog.OpenFile()) != null)
+                {
+
+
+                    myStream.Close();
+                }
+                File.WriteAllBytes(saveFileDialog.FileName, decriptedFile);
+            }
+        }
+    }
+}
